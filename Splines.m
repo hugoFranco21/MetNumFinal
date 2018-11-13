1824688efb66dfd707b33aa67777a1d42a641dc9
hugoTec <hfranco21@hotmail.com>
2018-11-13 00:45:57 -0600

    grafica correctamente y le pide al usuario input

--- Splines.m
+++ Splines.m
@@ -18,12 +18,14 @@ close all
 % Ask for data
 DATA = input('Give me the data points as a 2 column matrix ');
 [m,n] = size(DATA);
+if m < 2
+    disp(' To use splines you need at least 2 points in the plane ');
+    return
+end
 if n == 2
     %Order the data
     DATA = sortrows(DATA);
-    % Graph of the points
-    plot(DATA(:,1),DATA(:,2), 'o')
-    hold on
+    
     
     % Calculate and fill the vector of increments
     h = zeros(m-1,1);
@@ -59,14 +61,39 @@ if n == 2
     %Create and initialize with 0 vector sigma
     sigma = 0;
     sig = coeff\con;
-    sigma = [sigma;sig;sigma]
+    sigma = [sigma;sig;sigma];
     %fun = zeros(m,1);
-    q = cell(m-1,1)
+    q = cell(m-1,1);
     for i = 1:m-1
-        q{i,1} = @(x) (sigma(i)/6)*(((DATA(i+1,1)-x)^3)/h(i)-h(i)*((DATA(i+1,1)-x))) + (sigma(i+1)/6)*(((x-DATA(i,1))^3)/h(i)-h(i)*((x-DATA(i,1)))) + DATA(i,2)*((DATA(i+1,1)-x)/h(i))+ DATA(i+1,2)*((x-DATA(i,1))/h(i))
+        q{i,1} = @(x) (sigma(i)/6)*(((DATA(i+1,1)-x).^3)/h(i)-h(i)*((DATA(i+1,1)-x))) + (sigma(i+1)/6)*(((x-DATA(i,1)).^3)/h(i)-h(i)*((x-DATA(i,1)))) + DATA(i,2)*((DATA(i+1,1)-x)/h(i))+ DATA(i+1,2)*((x-DATA(i,1))/h(i));
         
     end
+    opc = 1;
     
+    while opc == 1
+        con = 1;
+        ind = 2;
+        xu = input(' Write x value in which you want to see the value of the approximation: ');
+        %Check the polynomial that xu has to use
+        while xu > DATA(ind,1)
+            con = con + 1;
+            ind = ind + 1;
+        end
+        yu = q{con}(xu);
+        plot(xu,yu,'o')
+        hold on
+        % Graph the spline
+        for i = 1:m-1
+            x1 = linspace(DATA(i,1),DATA(i+1,1));
+            y1 = q{i}(x1);
+            % Graph of the points
+            plot(DATA(:,1),DATA(:,2), 'x')
+            hold on
+            plot(x1,y1,'m')
+            hold on
+        end
+        opc = input(' Do you want another aproximation? (1 for Yes, 0 for No) ');
+    end
 else
     disp('ERROR: Matrix does not have 2 columns')
     return
